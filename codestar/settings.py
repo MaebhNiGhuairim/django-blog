6befd5ccfb09dbce5f44879e06d17c80b6fdb895
Maebh Ni <mnighuairim@gmail.com>
2025-01-09 10:23:26 +0000

    changed debug to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 # ".herokuapp.com"
 # 'https://my-first-app-e1802fa6d83b.herokuapp.com/'
 
