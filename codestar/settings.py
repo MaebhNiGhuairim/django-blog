45c4bc7b1d9583fc76f257db9f7b3efe1c005184
Maebh Ni <mnighuairim@gmail.com>
2025-01-10 15:42:27 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 # below is a potential fix for bad heroku UI
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
